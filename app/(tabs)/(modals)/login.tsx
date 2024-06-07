3b1eb2fc0c38dbd993f40351b8111bb85fc20b99
krishna jain <krishnajain5050@gmail.com>
2024-06-07 19:01:50 +0530

    default styles setup

--- app/(tabs)/(modals)/login.tsx
+++ app/(tabs)/(modals)/login.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
+    <View className='flex-1 bg-white p-6'>
+      <TextInput
+        
+      />
     </View>
   )
 }
